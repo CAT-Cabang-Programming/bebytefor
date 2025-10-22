3b6e3af319d42947dc45ef4cce4877439c62d4d5
MadZMana <notmadz132@gmail.com>
2025-10-22 13:07:35 +0700

    Use cors

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ import dotenv from "dotenv";
 import threadController from "./src/thread/thread.controller.js";
 import userController from "./src/user/user.controller.js";
 import commentController from "./src/comment/comment.controller.js";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 
+app.use(cors());
 app.use(express.json());
+
 app.use("/threads", threadController);
 app.use("/users", userController);
 app.use("/comments", commentController);
